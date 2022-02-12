3383dacbdb3ee891ae58631b70d5e70b40ec544a
millross131 <99500193+millross131@users.noreply.github.com>
2022-02-12 18:59:42 -0500

    Update partb.c

--- partb.c
+++ partb.c
@@ -1,23 +1,24 @@
 /*
- * 
+ * Lab 2 draft 4
  *
  * Created: 1/29/2022 11:07:43 AM
  * Author : Ross Miller
  */ 
-#define F_CPU 16000000UL
-#include <avr/io.h>
 #include <util/delay.h>
+#define F_CPU 16000000UL
 #include <avr/interrupt.h>
+#include <avr/io.h>
+
 
 int i = 0;
+
 void Initialize(){
 	cli();
-	DDRB &= ~(1<<DDB0);
 	DDRB |= (1<<DDB1);
 	DDRB |= (1<<DDB2);
 	DDRB |= (1<<DDB3);
 	DDRB |= (1<<DDB4);
-	
+	DDRB &= ~(1<<DDB0);
 
 	TCCR1B &= ~(1<<WGM12);
 	TCCR1B &= ~(1<<WGM13);
@@ -31,14 +32,15 @@ void Initialize(){
 }
 
 ISR(TIMER1_CAPT_vect){
-	PORTB ^= (1<<PORTB1);
+	
 	TCCR1B ^= (1<<ICES1);
+	PORTB ^= (1<<PORTB1);
+
 }
 
 int main(void)
 {
 	Initialize();
-	/* Replace with your application code */
 	while (1)
 	{
 	}
